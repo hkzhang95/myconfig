cca9fdbe3b4ce17f1bf5a1e7fd42affcff5de92a
GodBlessZhk <GodBlessZhk@outlook.com>
2019-06-13 17:14:10 +0800

    update YCM install setup

--- setup.sh
+++ setup.sh
@@ -10,4 +10,5 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
+git submodule update --init --recursive
 ./install.py --clang-completer
