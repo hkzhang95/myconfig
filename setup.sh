ea67083aa73b55202169b56a61403a27f6d2cf9a
GodBlessZhk <GodBlessZhk@outlook.com>
2019-06-21 07:52:45 +0800

    add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -12,3 +12,10 @@ vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
 ./install.py --clang-completer
+
+# setting on-my-zsh
+curl -Lo ~/oh-my-zsh_install.sh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh
+sh ~/oh-my-zsh_install.sh --unattended
+chsh -s $(which zsh)
+echo "oh-my-zsh installed, remember to modify ~/.zshrc and restart your terminal"
+rm ~/oh-my-zsh_install.sh
